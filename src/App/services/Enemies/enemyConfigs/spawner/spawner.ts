c89de57446cd59a06e42d21811601528f234bada
Fredrik Hjärner <frederick.hjarner@gmail.com>
2022-06-12 00:29:04 +0200

    not broken.

--- src/App/services/Enemies/enemyConfigs/spawner/spawner.ts
+++ src/App/services/Enemies/enemyConfigs/spawner/spawner.ts
@@ -13,96 +13,28 @@ export const spawner: IEnemyJson = {
    actions: [
       { type: 'wait', frames: 120 },
       { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 128.5, y: -22 } },
-      { type: 'wait', frames: 0 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 228.5, y: -22 }
-      },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 228.5, y: -22 } },
       { type: 'wait', frames: 40 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 128.5, y: -22 }
-      },
-      { type: 'wait', frames: 0 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 228.5, y: -22 }
-      },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 128.5, y: -22 } },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 228.5, y: -22 } },
       { type: 'wait', frames: 40 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 128.5, y: -22 }
-      },
-      { type: 'wait', frames: 0 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 228.5, y: -22 }
-      },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 128.5, y: -22 } },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 228.5, y: -22 } },
       { type: 'wait', frames: 40 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 128.5, y: -22 }
-      },
-      { type: 'wait', frames: 0 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 228.5, y: -22 }
-      },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 128.5, y: -22 } },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 228.5, y: -22 } },
       { type: 'wait', frames: 40 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 128.5, y: -22 }
-      },
-      { type: 'wait', frames: 0 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 228.5, y: -22 }
-      },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 128.5, y: -22 } },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 228.5, y: -22 } },
       { type: 'wait', frames: 40 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 128.5, y: -22 }
-      },
-      { type: 'wait', frames: 0 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 228.5, y: -22 }
-      },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 128.5, y: -22 } },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 228.5, y: -22 } },
       { type: 'wait', frames: 40 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 128.5, y: -22 }
-      },
-      { type: 'wait', frames: 0 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 228.5, y: -22 }
-      },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 128.5, y: -22 } },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 228.5, y: -22 } },
       { type: 'wait', frames: 40 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 128.5, y: -22 }
-      },
-      { type: 'wait', frames: 0 },
-      {
-         type: 'spawn',
-         enemy: 'nonShootingAimer',
-         position: { x: 228.5, y: -22 }
-      },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 128.5, y: -22 } },
+      { type: 'spawn', enemy: 'nonShootingAimer', position: { x: 228.5, y: -22 } },
       { type: 'wait', frames: 160 },
       { type: 'spawn', enemy: 'sinusLeft', position: { x: 75, y: -20 } },
       { type: 'wait', frames: 70 },
@@ -124,16 +56,7 @@ export const spawner: IEnemyJson = {
       { type: 'wait', frames: 70 },
       { type: 'spawn', enemy: 'sinusRight', position: { x: 280, y: -20 } },
       { type: 'wait', frames: 250 },
-      {
-         type: 'spawn',
-         enemy: 'firstMiniboss1',
-         position: { x: 118.881, y: -20 }
-      },
-      { type: 'wait', frames: 0 },
-      {
-         type: 'spawn',
-         enemy: 'firstMiniboss2',
-         position: { x: 237.762, y: -20 }
-      }
+      { type: 'spawn', enemy: 'firstMiniboss1', position: { x: 118.881, y: -20 } },
+      { type: 'spawn', enemy: 'firstMiniboss2', position: { x: 237.762, y: -20 } }
    ]
 };
