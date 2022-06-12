9558655df6434c0c22f24dc9c48446d3cc51a4d3
Fredrik Hjärner <frederick.hjarner@gmail.com>
2022-06-12 02:04:48 +0200

    not broken. made a TShortFormSpawn to compress json/yaml files.

--- src/App/services/Enemies/actionTypesShortForms.ts
+++ src/App/services/Enemies/actionTypesShortForms.ts
@@ -6,13 +6,23 @@ const isShortFormWait = (action: TShortFormAction): action is TShortFormWait => 
    return (action as TShortFormWait).wait !== undefined;
 };
 
+export type TShortFormSpawn = { spawn: string, x: number, y: number };
+
+const isShortFormSpawn = (action: TShortFormAction): action is TShortFormSpawn => {
+   return (action as TShortFormSpawn).spawn !== undefined;
+};
+
 export const ShortFormToLongForm = (shortForm: TShortFormAction): TAction => {
    if(isShortFormWait(shortForm)) {
-      return { type: 'wait', frames: shortForm.wait };
+      const { wait } = shortForm;
+      return { type: 'wait', frames: wait };
+   } else if(isShortFormSpawn(shortForm)) {
+      const { spawn, x, y } = shortForm;
+      return { type: 'spawn', enemy: spawn, x, y };
    }
    return shortForm;
 };
 
-export type TShortFormAction =
-   TAction |
-   TShortFormWait;
+export type TShortFormAction = TAction |
+   TShortFormWait |
+   TShortFormSpawn;
