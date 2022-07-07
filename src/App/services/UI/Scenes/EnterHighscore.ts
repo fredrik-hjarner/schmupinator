dd5dcf5b199fdd48a5388070aa15fffd52445609
Fredrik Hjärner <frederick.hjarner@gmail.com>
2022-07-07 13:21:33 +0200

    not broken.

---
+++ src/App/services/UI/Scenes/EnterHighscore.ts
@@ -0,0 +1,57 @@
+import type { IScene } from "./IScene";
+import type { UI } from "../UI";
+
+import { BrowserDriver } from "../../../../drivers/BrowserDriver";
+import { resolutionHeight, resolutionWidth, zIndices } from "../../../../consts";
+import { px } from "../../../../utils/px";
+
+type TConstructor = {
+   ui: UI;
+}
+
+export class EnterHighscore implements IScene {
+   readonly ui: UI;
+   private shadeElement?: HTMLDivElement;
+
+   constructor(params: TConstructor) {
+      this.ui = params.ui;
+   }
+
+   public render() {
+      this.createShade();
+   }
+
+   public destroy() {
+      this.shadeElement?.remove();
+      this.shadeElement = undefined;
+   }
+
+   // TODO: reduce this being created for all scenes.
+   private createShade = () => {
+      BrowserDriver.WithWindow(window => {
+         const element = window.document.createElement("div");
+         this.shadeElement = element;
+
+         const text = window.document.createTextNode("Game over");
+         element.appendChild(text);
+
+         element.style.lineHeight = px(resolutionHeight);
+         element.style.color = "white";
+         element.style.textAlign = "center";
+         element.style.fontSize = px(27);
+
+         // element.id = handle;
+         element.style.position = "fixed";
+         element.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
+         element.style.top = px(0);
+         element.style.left = px(0);
+         element.style.width = px(resolutionWidth);
+         element.style.height = px(resolutionHeight);
+         element.style.zIndex = zIndices.ui;
+
+         window.document.body.appendChild(element);
+         
+         return element;
+      });
+   };
+}
