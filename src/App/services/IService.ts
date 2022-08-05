e1722ba3440d87b09467851410439995a07d6c16
Fredrik Hjärner <frederick.hjarner@gmail.com>
2022-08-05 17:08:55 +0200

    not broken.

--- src/App/services/IService.ts
+++ src/App/services/IService.ts
@@ -1,10 +1,43 @@
+import type { IEventsPoints, IGameEvents, IUiEvents } from "./Events/IEvents";
 import type { Settings } from "./Settings/Settings";
 import type { IInput } from "./Input/IInput";
+import type { IE2eTest } from "./E2eTest/IE2eTest";
+import type { IGameLoop } from "./GameLoop/IGameLoop";
+import type { IFps } from "./Fps/IFps";
+import type { Enemies } from "./Enemies/Enemies";
+import type { GamePad } from "./GamePad/GamePad";
+import type { Collisions } from "./Collisions/Collisions";
+import type { IGameSpeed } from "./GameSpeed/IGameSpeed";
+import type { IPoints } from "./Points/IPoints";
+import type { Highscore } from "./Highscore/Highscore";
+import type { Yaml } from "./Yaml/Yaml";
+import type { IGraphics } from "./Graphics/IGraphics";
+import type { IUI } from "./UI/IUI";
+import type { IFullscreen } from "./Fullscreen/IFullscreen";
+import type { IParallax } from "./Parallax/IParallax";
+import type { IOutsideHider } from "./OutsideHider/IOutsideHider";
 
 export type TInitParams = Partial<{
+  e2eTest: IE2eTest;
   settings: Settings;
   input: IInput;
-  [serviceName: string]: IService;
+  gameLoop: IGameLoop;
+  fps: IFps;
+  enemies: Enemies;
+  gamepad: GamePad;
+  collisions: Collisions;
+  events: IGameEvents;
+  eventsUi: IUiEvents;
+  eventsPoints: IEventsPoints;
+  gameSpeed: IGameSpeed;
+  points: IPoints;
+  highscore: Highscore;
+  yaml: Yaml;
+  graphics: IGraphics;
+  ui: IUI;
+  fullscreen: IFullscreen;
+  parallax: IParallax;
+  outsideHider: IOutsideHider;
 }>;
 
 export interface IService {
